cb4c6fd29896cfe41db3c04bc136db7fc51d1302
xiazhi <49071835+xiazhi550@users.noreply.github.com>
2023-07-07 16:59:09 +0000

    test1

--- test.go
+++ test.go
@@ -2,8 +2,29 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
+func sqrt(x float64) float64 {
+	var z float64 = 1
+	var d float64
+	for i := 0; i < 10; i++ {
+		d = (z*z - x) / (2 * z)
+		z -= d
+		fmt.Println(z)
+		if math.Abs(d) < 1e-6 {
+			break
+		}
+	}
+	return z
+}
+
 func main() {
-	fmt.Println("hello")
+	fmt.Println("counting")
+
+	for i := 0; i < 10; i++ {
+		defer fmt.Println(i)
+	}
+
+	fmt.Println("done")
 }
